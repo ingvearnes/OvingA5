d60d7c68fea08c3b84db49510bf7ec309ac174b6
Ingve Værnes <ingve09@gmail.com>
2025-03-06 10:27:15 +0100

    setup tests

--- src/main/java/no/ntnu/oving5/ovinga5/HelloApplication.java
+++ src/main/java/no/ntnu/oving5/ovinga5/HelloApplication.java
@@ -1,5 +1,15 @@
 package no.ntnu.oving5.ovinga5;
 
+/*
+Easiest way to add github for mac:
+1. Gå på github, lag ny repository UTEN readMe eller noen annet (så tomt som mulig)
+2. På lokalt prosjekts filplassering i terminal: git init -b main/master
+3. git add .; git commit -m "tekst"
+4. git remote add origin URL
+5. Sjekk om det virket med; git remote -v
+6. git push -u origin main
+ */
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
---
+++ src/test/java/no/ntnu/oving5/ovinga5/HelloControllerTest.java
@@ -0,0 +1,12 @@
+package no.ntnu.oving5.ovinga5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloControllerTest {
+
+  @Test
+  void onHelloButtonClick() {
+  }
+}
